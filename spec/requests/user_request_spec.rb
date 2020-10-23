2f8ca941b8fdc5a8575104644801bfee3c0eadfc
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-10-23 12:28:07 -0400

    tested route user route

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,5 +1,24 @@
+# Tests the UsersController
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe 'Users Controller', type: :request do
+  # Create 10 Users and save them to the database
+  let!(:users) { create_list(:user, 10) }
 
+  # Tests the GET /users route
+  describe 'GET /users' do
+
+    before { get '/users' }
+
+    context 'when records exist' do
+      it 'returns users' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(10)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
