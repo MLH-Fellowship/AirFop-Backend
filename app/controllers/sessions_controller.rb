ca48ea3dc5651b2901b0e4072b370c198fd47108
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-11-12 13:10:20 -0500

    implemented a basic session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 # Class manages the session for a user
 class SessionsController < ApplicationController
 
-  def new
-  end
-
+  # Creates a new session
   def create
+    user = User.find_by(email: params[:email].downcase) # TODO may need to rethink this
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+    else
+      print 'Error' # TODO send some sort of error
+    end
   end
 
+  # Ends the current session
   def destroy
+    session[:user_id] = nil
   end
 end
