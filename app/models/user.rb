81eccc6544ebaf237d4dedc3ac2df900c7b5ccfa
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-11-18 12:36:27 -0500

    removed user model and user controller

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  validates_presence_of :first_name, :last_name, :email, :password_digest
-  validates :email, uniqueness: { case_sensitive: false }
-end
