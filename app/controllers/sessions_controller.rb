6c0661995f6dc98d6c2a71f9a51511e2d92725a6
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-11-19 09:14:03 -0500

    created a basic session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
+# Class handles user sessions
 class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_email! params[:email]
+
+    if user&.valid_password?(params[:password])
+      render json: user.as_json(only: %i[id email]), status: :created
+    else
+      head :unauthorized
+    end
+  end
 end
