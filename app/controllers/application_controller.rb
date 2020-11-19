a46ee518ac5ad955ae8fa06b78e5164ec8529db1
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-11-19 14:16:50 -0500

    added application header checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  private
+
+  # Returns the current user
+  def current_user
+    @current_user = User.find_by_authentication_token! params[:authentication_token]
+  end
+
+  # Returns if the current user is logged in or not
+  def logged_in?
+    !!current_user
+  end
 end
