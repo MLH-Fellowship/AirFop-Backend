f616e1eea4ff67db46b96da626bf678506ee0fcf
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-11-20 08:05:53 -0500

    implemented destroy session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,9 @@
 # Class handles user sessions
 class SessionsController < ApplicationController
 
+  before_action :set_user
+
+  # Creates a new session for a user
   def create
     user = User.find_by_email! params[:email]
 
@@ -9,5 +12,27 @@ class SessionsController < ApplicationController
     else
       head :unauthorized
     end
+  end
+
+  # Destroys a session for a user
+  def destroy
+    if @user
+      @user.authentication_token = nil
+      if @user.save
+        head :destroy
+      else
+        head :bad_request
+      end
+    else
+      head :unauthorized
+    end
+  end
+
+
+  private
+
+
+  def set_user
+    @user = User.find_by_email! params[:email]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  devise_for :users
+  resources :users, only: %i[create show update]
+  # devise_for :users
 
   resources :sessions, only: %i[create destroy]
 end
