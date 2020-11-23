f12f8dd6b913d4c947bfee3a01631a28f099eb68
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-11-23 17:04:14 -0500

    added is_admin to the User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :password)
+    params.permit(:first_name, :last_name, :email, :password, :is_admin)
   end
 end
