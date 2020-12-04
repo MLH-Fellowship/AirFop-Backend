28620748a808cac948974df69afaecf5148c9f71
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-12-04 12:40:14 -0500

    added User seed and Project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "John", last_name: "Doe", email: "airforce@us.gov", password: "password", is_admin: false)
+User.create(first_name: "Jane", last_name: "Stark", email: "airforce1@us.gov", password: "password", is_admin: true)
+User.create(first_name: "Johnny", last_name: "Timmy", email: "airforce2@us.gov", password: "password", is_admin: false)
+User.create(first_name: "Jacob", last_name: "Johnson", email: "airforce3@us.gov", password: "password", is_admin: true)
+User.create(first_name: "Joan", last_name: "Dark", email: "airforce4@us.gov", password: "password", is_admin: false)
+
+Project.create(project_num: 1, project_name: "Project One",
+                phase: "Phase One", award_date: Date.today, pop: "today", customer: "Batman", contractor: "Gotham",
+                pm: "Batgirl", status: "Green", status_comment: "Jim Gordon", funding_source: "Gotham Treasurey")
