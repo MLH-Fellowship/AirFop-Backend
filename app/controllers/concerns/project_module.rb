d2985cc846782629b24b1ef51088ce95125cd051
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-12-13 14:07:58 -0500

    fixing award_date bug

--- app/controllers/concerns/project_module.rb
+++ app/controllers/concerns/project_module.rb
@@ -1,22 +1,48 @@
 module ProjectModule
+  require 'time'
 
   def process_filter(params)
     filter_ops = params[:filter]
     if filter_ops
-      status_ops = params[:filter][:status]
-      unless status_ops.nil?
-        ops_string = ''
-        status_ops.each do |key, value|
-          next unless value.downcase == 'true'
-
-          ops_string << 'or ' if ops_string != ''
-          ops_string << "status = '#{key}' "
-        end
-        unless ops_string.empty?
-          @project = Project.where(ops_string)
-          json_response(@project)
-        end
+      ops_string = gen_status_string(params[:filter][:status])
+      date_string = params[:filter][:date]
+      date_present = false
+      start_time = nil
+      end_time = nil
+      if date_string != 'null' && !date_string.nil?
+        date_present = true
+        start_time = Time.parse(params[:filter][:date][:start])
+        end_time = Time.parse(params[:filter][:date][:end])
       end
+
+      @project = nil
+      if !ops_string.empty? && date_present
+        @project = Project.where(ops_string, award_date: start_time..end_time)
+      end
+
+      unless ops_string.empty?
+        @project = Project.where(ops_string)
+        json_response(@project)
+      end
+    end
+  end
+
+  # Generate the SQL string for status
+  def gen_status_string(status_ops)
+    ops_string = ''
+    status_ops&.each do |key, value|
+      next unless value.downcase == 'true'
+
+      ops_string << 'or ' if ops_string != ''
+      ops_string << "status = '#{key}' "
+    end
+    ops_string
+  end
+
+  def gen_date_string(date_ops)
+    ops_string = ''
+    date_ops&.each do |key, value|
+
     end
   end
 end
