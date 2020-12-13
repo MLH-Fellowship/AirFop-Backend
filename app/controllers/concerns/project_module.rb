70ad9a4a6f2daba3c175db34c1765d34054f8f19
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-12-13 16:37:10 -0500

    completed query functinality

--- app/controllers/concerns/project_module.rb
+++ app/controllers/concerns/project_module.rb
@@ -1,3 +1,4 @@
+# Functions for the Projects controller
 module ProjectModule
   require 'time'
 
@@ -9,6 +10,7 @@ module ProjectModule
       date_present = false
       start_time = nil
       end_time = nil
+      # Processes the dates
       if date_string != 'null' && !date_string.nil?
         puts 'Inside of the time statement'
         date_present = true
@@ -17,15 +19,15 @@ module ProjectModule
       end
 
       @project = nil
-      if !ops_string.empty? && date_present
-        puts 'Inside of the final selection'
+      if !ops_string.nil? && !ops_string.empty? && date_present
         @project = Project.where(ops_string).where(award_date: start_time..end_time)
+      elsif !ops_string.nil? && !ops_string.empty?
+        @project = Project.where(ops_string)
+      elsif date_present
+        @project = Project.where(award_date: start_time..end_time)
+      else
+        puts 'There was an error in the query selection'
       end
-
-      # unless ops_string.empty?
-      #   @project = Project.where(ops_string)
-      #   json_response(@project)
-      # end
       json_response(@project)
     end
   end
@@ -33,19 +35,14 @@ module ProjectModule
   # Generate the SQL string for status
   def gen_status_string(status_ops)
     ops_string = ''
-    status_ops&.each do |key, value|
-      next unless value.downcase == 'true'
+    if status_ops != 'null'
+      status_ops&.each do |key, value|
+        next unless value.downcase == 'true'
 
-      ops_string << 'or ' if ops_string != ''
-      ops_string << "status = '#{key}' "
-    end
-    ops_string
-  end
-
-  def gen_date_string(date_ops)
-    ops_string = ''
-    date_ops&.each do |key, value|
-
+        ops_string << 'or ' if ops_string != ''
+        ops_string << "status = '#{key}' "
+      end
+      ops_string
     end
   end
 end
