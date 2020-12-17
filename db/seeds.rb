af276c82fd29e10bf78fd91ad47f8c03ff98a074
Dena Burd <denaburd@gmail.com>
2020-12-17 05:34:24 -0800

    make seed data more accurate

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,28 +12,28 @@ User.create(first_name: 'Johnny', last_name: 'Timmy', email: 'airforce2@us.gov',
 User.create(first_name: 'Jacob', last_name: 'Johnson', email: 'airforce3@us.gov', password: 'password', is_admin: true)
 User.create(first_name: 'Joan', last_name: 'Dark', email: 'airforce4@us.gov', password: 'password', is_admin: false)
 
-Project.create(project_num: 1, project_name: 'Project One',
-               phase: 'Phase One', award_date: Date.today, pop: 'today', customer: 'Batman', contractor: 'Gotham',
+Project.create(project_num: 1, project_name: 'ProjectOne',
+               phase: 'Exclusion', award_date: Date.today, pop: 'today', customer: 'Batman', contractor: 'Gotham',
                pm: 'Batgirl', status: 'Green', status_comment: 'Jim Gordon', funding_source: 'Gotham Treasurey',
-               name: 'Project00021111One')
+               name: 'Project One')
 
-Project.create(project_num: 2, project_name: 'Project Two',
-               phase: 'Phase Two', award_date: Date.today, pop: 'today', customer: 'Superman', contractor: 'Earth',
+Project.create(project_num: 2, project_name: 'ProjectTwo',
+               phase: 'Pre-award', award_date: Date.today, pop: 'today', customer: 'Superman', contractor: 'Earth',
                pm: 'Louis Lane', status: 'Red', status_comment: 'Maybe guy in glasses?', funding_source: 'US Treasury',
-               name: 'Project000164211Two')
+               name: 'Project Two')
 
-Project.create(project_num: 3, project_name: 'Project Three',
-               phase: 'Phase Three', award_date: Date.today, pop: 'today', customer: 'Captian America',
+Project.create(project_num: 3, project_name: 'ProjectThree',
+               phase: 'Pre-award', award_date: Date.today, pop: 'today', customer: 'Captian America',
                contractor: 'Shield', pm: 'Black Widow', status: 'Red', status_comment: 'First Avenger',
-               funding_source: 'Nick Fury', name: 'Project1561651Three')
+               funding_source: 'Nick Fury', name: 'Project Three')
 
-Project.create(project_num: 4, project_name: 'Project Four',
-               phase: 'Phase One', award_date: Date.today, pop: 'today', customer: 'Iron Man',
+Project.create(project_num: 4, project_name: 'ProjectFour',
+               phase: 'Closeout', award_date: Date.today, pop: 'today', customer: 'Iron Man',
                contractor: 'Depeartment of Defense', pm: 'Pepper Potts', status: 'Yellow', status_comment: '',
-               funding_source: 'Stark Industries', name: 'Project000001111Four')
+               funding_source: 'Stark Industries', name: 'Projec Four')
 
-Project.create(project_num: 5, project_name: 'Project Five',
-               phase: 'Phase Three', award_date: Date.today, pop: 'today', customer: 'Spiderman', contractor: 'Zendaya',
+Project.create(project_num: 5, project_name: 'ProjectFive',
+               phase: 'Closeout', award_date: Date.today, pop: 'today', customer: 'Spiderman', contractor: 'Zendaya',
                pm: 'Iron Man', status: 'Yellow', status_comment: 'Friendly Neighborhood',
-               funding_source: 'Stark Industries', name: 'Project005471Five')
+               funding_source: 'Stark Industries', name: 'Project Five')
 
