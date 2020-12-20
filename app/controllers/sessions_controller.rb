3d609f18ff9ef989290cea0b1499f04e3451c933
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-12-20 14:26:58 -0500

    changed data passed during session starts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,8 @@ class SessionsController < ApplicationController
 
   # Creates a new session for a user
   def create
-    user = User.find_by_email! params[:email]
-
-    if user&.valid_password?(params[:password])
-      render json: user.as_json(only: %i[id email authentication_token]), status: :created
+    if @user&.valid_password?(params[:password])
+      render json: @user.as_json(only: %i[first_name last_name email authentication_token]), status: :created
     else
       head :unauthorized
     end
