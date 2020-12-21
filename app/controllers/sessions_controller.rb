a0bf21dc66c197929d528483beb803bfba2472e0
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-12-21 10:48:31 -0500

    possible fix for jwt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 # Class handles user sessions
 class SessionsController < ApplicationController
 
+  require 'jwt'
+
   # Creates a new session for a user
   def create
+    hmac_secret = 'ThisIsASecret'
     @user = User.find_by_email! params[:email]
     if @user&.valid_password?(params[:password])
-      render json: @user.as_json(only: %i[first_name last_name email authentication_token]), status: :created
+      payload = @user.as_json(only: %i[first_name last_name email authentication_token])
+      token = JWT.encode payload, hmac_secret, 'HS256'
+      puts token
+      render json: token
+      # render json: @user.as_json(only: %i[first_name last_name email authentication_token]), status: :created
     else
       head :unauthorized
     end
